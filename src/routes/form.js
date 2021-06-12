ee48d16e31a489ab7a45e8c11db7702f440eef6a
jordonezp <jordonez@uc.cl>
2021-06-12 19:08:13 -0400

    vista del formulario de recomendación completado

--- src/routes/form.js
+++ src/routes/form.js
@@ -14,6 +14,7 @@ router.get('form.results', '/show', async (ctx) => {
     const norte = ["I" ,"II" ,"III" ,"IV", "XV"];
     const centro = ["V" ,"VI" ,"VII" ,"VIII", "IX","XIII" ,"XIV","XVI"];
     const sur = ["X" ,"XI" ,"XII"];
+    // Incluir paneles solares LED en la recomendación
     if ((centro.includes(categories.region) || norte.includes(categories.region)) && 
         (categories.homeType == "Casa") && 
         (categories.spaceAvailable == true) && 
@@ -22,17 +23,17 @@ router.get('form.results', '/show', async (ctx) => {
         (!categories.products.includes("Paneles Fotovoltaicos"))) {
         recomendations.push("Paneles Fotovoltaicos");
     };
-    
+    // Incluir ampolletas LED en la recomendación
     if ((categories.lightBulbQuantity >= 20) && 
         (categories.lightBulbType != "LED")) {
         recomendations.push("Ampolletas LED");
     };
-
+    // Incluir refrigeradores en la recomendación
     if (!categories.products.includes("Refrigeradores")) {
         recomendations.push("Refrigeradores")
     };
     
-
+    // Se entregan las categorías recomendadas junto con el precio mínimo de los productos
     await ctx.render('form/show', {
         categories,
         recomendations
