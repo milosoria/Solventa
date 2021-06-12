f4f3c23c3eaad49d30e6d46771b8d6c251742059
jordonezp <jordonez@uc.cl>
2021-06-12 17:14:24 -0400

    Avance en el fomrulario de recomendación

---
+++ src/routes/form.js
@@ -0,0 +1,42 @@
+const KoaRouter = require('koa-router');
+
+const router = new KoaRouter();
+
+router.get('form.show', '/new', async (ctx) => {
+    await ctx.render('form/new', {
+        sendFormPath: () => ctx.router.url('form.results'), 
+    })
+    });
+
+router.get('form.results', '/show', async (ctx) => {
+    const categories = ctx.params;
+    const recomendations = [];
+    const norte = ["I" ,"II" ,"III" ,"IV", "XV"];
+    const centro = ["V" ,"VI" ,"VII" ,"VIII", "IX","XIII" ,"XIV","XVI"];
+    const sur = ["X" ,"XI" ,"XII"];
+    if ((centro.includes(categories.region) || norte.includes(categories.region)) && 
+        (categories.homeType == "Casa") && 
+        (categories.spaceAvailable == true) && 
+        (categories.timeAtHome.includes("Mañana") && categories.timeAtHome.includes("Tarde")) &&
+        (categories.propertyArea >= 400) && 
+        (!categories.products.includes("Paneles Fotovoltaicos"))) {
+        recomendations.push("Paneles Fotovoltaicos");
+    };
+    
+    if ((categories.lightBulbQuantity >= 20) && 
+        (categories.lightBulbType != "LED")) {
+        recomendations.push("Ampolletas LED");
+    };
+
+    if (!categories.products.includes("Refrigeradores")) {
+        recomendations.push("Refrigeradores")
+    };
+    
+
+    await ctx.render('form/show', {
+        categories,
+        recomendations
+    })
+    });
+
+module.exports = router;
