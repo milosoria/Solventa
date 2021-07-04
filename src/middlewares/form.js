fd3dfb49179c3a11b55fa46d70b8439e356e75ef
milosoria <camilo.soria@uc.cl>
2021-07-03 20:40:38 -0400

    big fixes

--- src/middlewares/form.js
+++ src/middlewares/form.js
@@ -1,5 +1,15 @@
-function getRecomendations(formData, serviceList) {
-    return recomendations
+function compare(a,b) {
+    if (a.rating > b.rating && a.minReturn > b.minReturn && a.price > b.price || a.rating > b.rating) {
+        return 1
+    }
+    if (a.rating < b.rating && a.minReturn < b.minReturn && a.price < b.price || a.rating < b.rating ){
+        return -1
+    }
+    return 0
+}
+function getRecomendations(serviceList) {
+    serviceList.sort((a,b) => compare(a,b));
+    return serviceList
 }
 
 module.exports = {
